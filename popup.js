9bc104dbc32cbca5bd65f8caabbe2a3b2d56e33e
RKintanar <Ryan.Kintanar@gmail.com>
2017-02-19 17:54:13 +0800

    Initial Commit
    Version 0.1

---
+++ popup.js
@@ -0,0 +1,138 @@
+function handleClick(){
+	var searchterm = document.getElementById("textbox").value;
+	console.log(searchterm);
+	var resultsPage=document.getElementById("results");
+	resultsPage.insertBefore(pfactDiv(searchterm), resultsPage.childNodes[0]);
+    return false; // prevent further bubbling of event
+}
+
+function pfactDiv(searchterm){
+	var obj = document.createElement('div');
+	var rawdata = lookupTerm(searchterm);
+	var parsedText = "";
+	var t = document.createTextNode(parsedText);
+	obj.appendChild(t);
+	for(var i=0;i<rawdata.objects.length;i++){
+		obj.innerHTML +=  "<strong>" + rawdata.objects[i].ruling_headline + ": " + rawdata.objects[i].ruling.ruling + "</strong>";
+		obj.innerHTML +="<br/>";
+		if(rawdata.objects[i].art[0].caption != rawdata.objects[i].ruling_headline){
+			obj.innerHTML +=  rawdata.objects[i].art[0].caption + " ";
+		}
+		obj.innerHTML += rawdata.objects[i].ruling_link_text ;
+		obj.innerHTML +="<br/>";
+		obj.innerHTML +=  "<img width=100px src='" + rawdata.objects[i].ruling.ruling_graphic + "' />";
+		obj.innerHTML +="<br/>";
+		obj.innerHTML +=  rawdata.objects[i].art[0].title;
+		obj.innerHTML +="<br/>";
+		var url = "http://www.politifact.com/" + rawdata.objects[i].canonical_url;
+		obj.innerHTML += "<a href='" + url + "'>" + url + "</a>";
+		obj.innerHTML +="<br/><br/><hr/>";
+	}
+	obj.setAttribute('class','result-item');
+	obj.style.backgroundColor = "#ffdddd";
+	obj.style.border= "1px solid black";
+	obj.style.padding= "20px";
+	obj.style.margin= "1px";
+	return obj;
+}
+
+function lookupTerm(headline) {
+	var callURL = "http://www.politifact.com/api/v/2/statement/?order_by=-ruling_date&limit=5&edition__edition_slug=truth-o-meter";
+	var speaker = getSpeakers(headline); //&speaker__name_slug=donald-trump
+	if (speaker != ""){
+		callURL +="&speaker__name_slug=" + speaker;
+	}
+	var subj = getSubjects(headline); //&subject__subject_slug=fake-new
+	if (subj != ""){
+		callURL +="&subject__subject_slug=" + subj;
+	}
+	callURL+="&format=json";
+	console.log("callURL " + callURL);
+	return httpGetAsync(callURL);
+}
+
+function httpGetAsync(theUrl) {
+	var xmlHttp = new XMLHttpRequest();
+	xmlHttp.onreadystatechange = function() {
+		if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+			return request.responseText;
+		}
+	}
+	xmlHttp.open("GET", theUrl, true); // true for asynchronous 
+	xmlHttp.send(null);
+}
+
+function getSpeakers(headline) {
+	console.log("Getspeakers " + headline);
+	var speakerSlugs = "";
+	if (headline.match(/trump|drumpf/i) != null) {
+		speakerSlugs += "donald-trump";
+	}
+	if (headline.match(/Rick Scott/i) != null) {
+		speakerSlugs += "rick-scott"
+	}
+	if (headline.match(/cuomo/i) != null) {
+		speakerSlugs += "andrew-cuomo"
+	}
+	if (headline.match(/learnprogress/i) != null) {
+		speakerSlugs += "learnprogressorg"
+	}
+	if (headline.match(/netanyahu/i) != null) {
+		speakerSlugs += "benjamin-netanyahu"
+	}
+	if (headline.match(/Bernie|Sanders/i) != null) {
+		speakerSlugs += "bernie-s"
+	}
+	if (headline.match(/stephen miller/i) != null) {
+		speakerSlugs += "stephen-miller"
+	}
+	return speakerSlugs;
+}
+
+function getSubjects(headline) {
+	var subjectSlug = "";
+	if (headline.match(/immigration|immigrant|deported|travel ban|leaks|cia|war on terror|terror/i) != null) {
+		subjectSlug += "homeland-security"
+	}
+	if (headline.match(/foreign policy|secretary of state|Iran|Iraq|Afghanistan|Israel|Brexit|EU|NATO/i) != null) {
+		subjectSlug += "foreign-policy"
+	}
+	if (headline.match(/job|employment|economy|stock market/i) != null) {
+		subjectSlug += "economy"
+	}
+	if (headline.match(/agriculture|farm/i) != null) {
+		subjectSlug += "agriculture"
+	}
+	if (headline.match(/pension/i) != null) {
+		subjectSlug += "pensions"
+	}
+	if (headline.match(/crime/i) != null) {
+		subjectSlug += "crime"
+	}
+	if (headline.match(/education|school|teacher|student|Common Core/i) != null) {
+		subjectSlug += "education"
+	}
+	if (headline.match(/Dakota Access Pipeline|climate change|global warming|epa/i) != null) {
+		subjectSlug += "climate-change"
+	}
+	if (headline.match(/congress|senate/i) != null) {
+		subjectSlug += "congress"
+	}
+	if (headline.match(/infrastructure|dam/i) != null) {
+		subjectSlug += "infrastructure"
+	}
+	if (headline.match(/fake news/i) != null) {
+		subjectSlug += "fake-news"
+	}
+	if (headline.match(/health|obamacare|ACA|insure/i) != null) {
+		subjectSlug += "health-care"
+	}
+	if (headline.match(/terrorism/i) != null) {
+		subjectSlug += "terrorism"
+	}
+	return subjectSlug;
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+	document.getElementById("submitbutton").addEventListener('click', handleClick);
+});
---
+++ readme.md
@@ -0,0 +1,7 @@
+Chrome Extension designed to fight fake news by adding reputable information to the Facebook feed.
+
+Scans the newsfeed for common politicians and news items (Donald Trump, healthcare, etc.) and inserts a div with qualified information below that.
+
+Right now only hitting politifact, and I'm getting rate limited by their API, but it does do what it's supposed to:
+
+![ScreenShot](/working.png)
